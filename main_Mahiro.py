f5af66ac312a759404f0a1653f779db399bbd9bd
meorinn <meiurawa3@gmail.com>
2024-10-18 16:33:07 +0900

    todo直しました壁あて

--- main_Mahiro.py
+++ main_Mahiro.py
@@ -66,6 +66,14 @@ def move(game_state: typing.Dict) -> typing.Dict:
     board_width = game_state['board']['width']
     board_height = game_state['board']['height']
     
+    if my_head["x"]==board_width-1:
+        is_move_safe["right"]=False
+    elif my_head["x"]==0:
+        is_move_safe["left"]=False
+    if my_head["y"]==board_width-1:
+        is_move_safe["up"]=False
+    elif my_head["y"]==0:
+        is_move_safe["down"]=False
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     # my_body = game_state['you']['body']
@@ -86,14 +94,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     # Choose a random move from the safe ones
     
-    if my_head["x"]==board_width-1:
-        is_move_safe["right"]=False
-    elif my_head["x"]==0:
-        is_move_safe["left"]=False
-    if my_head["y"]==board_width-1:
-        is_move_safe["up"]=False
-    elif my_head["y"]==0:
-        is_move_safe["down"]=False
+    
         
         
     next_move = random.choice(safe_moves)
