62d39145ccbbfcdb27e2d0a5def4f3a1297d11cb
TaketoUsui <take20030524@yahoo.co.jp>
2024-10-25 15:33:07 +0900

    [feat]自分にぶつからないようにした

--- main_Taketo.py
+++ main_Taketo.py
@@ -76,9 +76,27 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     my_body = game_state['you']['body']
+    for body in my_body:
+        if my_head["x"] == body["x"] and my_head["y"]+1 == body["y"]:
+            is_move_safe["up"] = False
+        if my_head["x"] == body["x"] and my_head["y"]-1 == body["y"]:
+            is_move_safe["down"] = False
+        if my_head["x"]+1 == body["x"] and my_head["y"] == body["y"]:
+            is_move_safe["right"] = False
+        if my_head["x"]-1 == body["x"] and my_head["y"] == body["y"]:
+            is_move_safe["left"] = False
 
     # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
+    opponents = game_state['board']['snakes']
+    for opponent in opponents:
+        if opponent['id'] == game_state['you']['id']:
+            continue
+        for body in opponent['body']:
+            if my_head["x"] == body["x"] and my_head["y"] == body["y"]:
+                is_move_safe["up"] = False
+                is_move_safe["down"] = False
+                is_move_safe["left"] = False
+                is_move_safe["right"] = False
 
     # Are there any safe moves left?
     safe_moves = []
@@ -91,11 +109,37 @@ def move(game_state: typing.Dict) -> typing.Dict:
         return {"move": "down"}
 
     # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
+    # next_move = random.choice(safe_moves)
 
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
+    next_move = None
 
+    food = game_state['board']['food']
+    nearest_food = None
+    health = game_state['you']['health']
+    board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    nearest_food_distance = board_height + board_width
+    for i in range(len(food)):
+        food_distance = abs(my_head["x"] - food[i]["x"]) + abs(my_head["y"] - food[i]["y"])
+        if nearest_food is None:
+            nearest_food = food[i]
+        elif food_distance < nearest_food_distance:
+            nearest_food = food[i]
+            nearest_food_distance = food_distance
+    if(health <= nearest_food_distance + 10):
+        if(nearest_food["x"] > my_head["x"] and is_move_safe["right"]):
+            next_move = "right"
+        elif(nearest_food["x"] < my_head["x"] and is_move_safe["left"]):
+            next_move = "left"
+        elif(nearest_food["y"] > my_head["y"] and is_move_safe["up"]):
+            next_move = "up"
+        elif(nearest_food["y"] < my_head["y"] and is_move_safe["down"]):
+            next_move = "down"
+    
+    if(next_move is None):
+        next_move = random.choice(safe_moves)
+        
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
 
